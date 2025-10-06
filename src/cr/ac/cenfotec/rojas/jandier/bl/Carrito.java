33abf6d3ebc64739fca92176abcbd7bf66af0d2b
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-06 17:27:43 -0600

    Tercer commit. Cambios significantes en Tienda y Carrito.java con base a sus metodos.

--- src/cr/ac/cenfotec/rojas/jandier/bl/Carrito.java
+++ src/cr/ac/cenfotec/rojas/jandier/bl/Carrito.java
@@ -8,15 +8,54 @@ import java.util.List;
 
 public class Carrito {
 
-    private List<Producto> productos = new ArrayList<>();
-    private int cantidadProducto;
-    private int numeroCarrito;
+    private List<Producto> miCarrito = new ArrayList<>();
+    private Tienda tienda = new Tienda();
+
     protected BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-
-    public void agregarProducto(Producto producto) throws IOException {
-
+    public List<Producto> getMiCarrito() {
+        return miCarrito;
     }
+
+
+    public boolean agregarProducto() throws IOException {
+
+        System.out.print("Ingrese el ID del producto que desea agregar: ");
+        int id = Integer.parseInt(in.readLine());
+
+        if (tienda.buscarProductoId(id) != null) {
+            miCarrito.add(tienda.buscarProductoId(id));
+            tienda.eliminarStock(id);
+            return true;
+        } else {
+            System.out.println("El producto con el ID = #" + id + " no existe");
+            return false;
+        }
+    }
+
+    public boolean eliminarProducto() throws IOException {
+
+        System.out.print("Ingrese el ID del producto dentro de su carrito que desea eliminar: ");
+        int id = Integer.parseInt(in.readLine());
+        if (miCarrito.contains(tienda.buscarProductoId(id))) {
+            miCarrito.remove(tienda.buscarProductoId(id));
+            tienda.devolverStock(id);
+            return true;
+        }else {
+            System.out.println("El producto con el ID = #" + id + " no existe en su carrito");
+            return false;
+        }
+    }
+
+    public void imprimirCarrito() {
+
+        System.out.println("Productos de mi carrito -->");
+        for (Producto producto : miCarrito) {
+            System.out.println("Producto: " + producto.getNombre() + " --> Stock: "
+                    + producto.getCantidad() + " --> Precio: $" + producto.getPrecio());
+        }
+    }
+
 
 
 
--- src/cr/ac/cenfotec/rojas/jandier/bl/Tienda.java
+++ src/cr/ac/cenfotec/rojas/jandier/bl/Tienda.java
@@ -1,16 +1,22 @@
 package cr.ac.cenfotec.rojas.jandier.bl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tienda {
 
      private List<Producto> productosStock;
+    private Carrito carrito = new Carrito();
 
 
     public Tienda() {
         productosStock = new ArrayList<>();
         agregarStock();
+    }
+
+    public List<Producto> getProductosStock() {
+        return productosStock;
     }
 
     public void agregarStock() {
@@ -21,11 +27,54 @@ public class Tienda {
         productosStock.add(new Producto("Queso", 7, 1005, 3400));
     }
 
+    public void eliminarStock(int id) throws IOException {
 
+        if (carrito.agregarProducto()) {
+            productosStock.remove(buscarProductoId(id));
+        }
+    }
+
+    public void devolverStock(int id) throws IOException {
+        if (carrito.eliminarProducto()) {
+            productosStock.add(buscarProductoId(id));
+        }
+    }
 
     public  void mostrarStock() {
         for (Producto producto : productosStock) {
             System.out.println( "[" + producto.getNombre() + "] cantidad -> " + producto.getCantidad() + " -> precio -> $" + producto.getPrecio());
         }
     }
+
+    public Producto buscarProductoId(int id) {
+        for (Producto producto : productosStock) {
+            if (producto.getId() == id) {
+                return producto;
+            }
+        }
+            return null;
+    }
+
+    public double escanearProductos() {
+        double total = 0;
+
+        //Cada producto de la lista miCarrito se le va a sumar el precio al total.
+        for (Producto producto : carrito.getMiCarrito()) {
+            total += producto.getPrecio();
+        }
+        return total;
+    }
+
+
+
+    public void imprimirFactura() {
+        System.out.println("-".repeat(10));
+        System.out.println("---Factura---");
+        System.out.println("-".repeat(10));
+        carrito.imprimirCarrito();
+        System.out.println("-".repeat(10));
+        System.out.println("Precio total: " + escanearProductos());
+        System.out.println("-".repeat(10));
+    }
+
 }
